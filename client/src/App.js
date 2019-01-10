bd2b7a5654ef34e391859f7455fc925fc5364681
rslol <richard.shelton567@gmail.com>
2019-01-10 15:43:00 -0500

    Initialized Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Navbar from './components/shared/Navbar';
 import Footer from './components/shared/Footer';
 import Login from './components/auth/Login';
@@ -7,18 +10,22 @@ import Register from './components/auth/Register';
 import Landing from './components/Landing';
 import './App.css';
 
+
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-          <Footer />
-        </div>
-      </Router>
+      <Provider store={ store }>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
